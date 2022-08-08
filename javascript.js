30b5c81fa8913f3ae1d949be92e43f4019d19369
randy <randy.thomas3.9393@gmail.com>
2022-08-08 06:28:38 +0100

    Create ComputerChoice Function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("rock")
+function getComputerChoice() {
+    let x, y ;
+    y = Math.floor(Math.random()*3+1)
+    if (x === 1){
+        return "rock"
+    }
+    else if (x === 2) {return "paper"}
+    else {return scissors}
+}
